978cc134db2f838fce8d7abc843abc934e124c46
Juan C Jimenez G <jcjimenez29@misena.edu.co>
2021-07-11 13:06:33 -0500

    [+] 02 Variables: Definicion de variables de tipo entero y cadena

---
+++ src/Variables.java
@@ -0,0 +1,17 @@
+// 02 Variables: Definicion de variables de tipo entero y cadena
+public class Variables {
+    public static void main( String args[] ) throws Exception {
+
+        //  Define variables de tipo entero
+        int dia = 13;
+        int anio = 1996;
+        //  Define variables de tipo cadena
+        String mes = "enero";
+        String mensaje = "Fecha del lanzamiento inicial de Java: ";
+
+        dia = 23;
+
+        System .out .println( mensaje + dia + " de " + mes + " de " + anio );
+
+    }
+}
