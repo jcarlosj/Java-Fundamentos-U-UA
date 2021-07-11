4d0d7fd2979f4556f5e69a3fa3f737ec3654653e
Juan C Jimenez G <jcjimenez29@misena.edu.co>
2021-07-11 14:42:01 -0500

    [+] 02 Variables: Concatenacion

--- src/Variables.java
+++ src/Variables.java
@@ -1,25 +1,18 @@
-// 02 Variables: Interferencia de tipos en Java
+// 02 Variables: Concatenacion de variables
 import java.lang.*;
 
 public class Variables {
     public static void main( String args[] ) throws Exception {
-        //  Define variables para inferir sus tipos, sin embargo posee restricciones en su uso (Disponible desde el JDK 10 en adelante)
-        var anio = 1996;
-        var mensaje = "Fecha del lanzamiento inicial de Java: ";
+        var i = 4;      // No permite: var i = 4, j = 5;
+        var j = 5;
+        String name = "Juan";
+        String profession = "Desarrollador";
 
-        //  Define explicitamente las variables
-        String mes = "enero";
-        int dia = 13;
-
-        dia = 23;
-
-        //  Obtenemos los tipos de datos de las variables usadas
-        System .out .println( "anio es: " + ( ( Object ) anio ) .getClass() .getSimpleName() );
-        System .out .println( "mensaje es: " + mensaje .getClass() .getSimpleName() );
-        System .out .println( "mes es: " + mes .getClass() .getSimpleName() );
-        System .out .println( "dia es: " + ( ( Object ) dia ) .getClass() .getSimpleName() +  "\n" );
-        //  Despliega el mensaje respectivo
-        System .out .println( mensaje + dia + " de " + mes + " de " + anio );
+        System .out .println( name + profession );      // JuanDesarrollador
+        System .out .println( i + j );                  // 9
+        System .out .println( i + j + name );           // 9Juan    Evalua de Izquierda a Derecha
+        System .out .println( name + i + j );           // Juan45   Contexto cadena: Si de derecha a izquierda primero encuentra una cadena, todo lo demas lo concaterara como cadena
+        System .out .println( name + ( i + j ) );       // Juan9    El contexto cadena se puede romper haciendo uso de los parentesis, aumentando la prioridad en las operaciones dentro de ellos
 
     }
 }
