47245f1d33330c4a444889d6de6163775a9dbfb0
Juan C Jimenez G <jcjimenez29@misena.edu.co>
2021-07-11 14:14:14 -0500

    [+] 02 Variables: Inferencia de tipos

--- src/Variables.java
+++ src/Variables.java
@@ -1,16 +1,24 @@
-// 02 Variables: Definicion de variables de tipo entero y cadena
+// 02 Variables: Interferencia de tipos en Java
+import java.lang.*;
+
 public class Variables {
     public static void main( String args[] ) throws Exception {
+        //  Define variables para inferir sus tipos, sin embargo posee restricciones en su uso (Disponible desde el JDK 10 en adelante)
+        var anio = 1996;
+        var mensaje = "Fecha del lanzamiento inicial de Java: ";
 
-        //  Define variables de tipo entero
-        int dia = 13;
-        int anio = 1996;
-        //  Define variables de tipo cadena
+        //  Define explicitamente las variables
         String mes = "enero";
-        String mensaje = "Fecha del lanzamiento inicial de Java: ";
+        int dia = 13;
 
         dia = 23;
 
+        //  Obtenemos los tipos de datos de las variables usadas
+        System .out .println( "anio es: " + ( ( Object ) anio ) .getClass() .getSimpleName() );
+        System .out .println( "mensaje es: " + mensaje .getClass() .getSimpleName() );
+        System .out .println( "mes es: " + mes .getClass() .getSimpleName() );
+        System .out .println( "dia es: " + ( ( Object ) dia ) .getClass() .getSimpleName() +  "\n" );
+        //  Despliega el mensaje respectivo
         System .out .println( mensaje + dia + " de " + mes + " de " + anio );
 
     }
