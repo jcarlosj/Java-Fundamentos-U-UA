9a28c8cdfcd7d37feb37b1d5646b21bf160195a3
Juan C Jimenez G <jcjimenez29@misena.edu.co>
2021-07-11 15:01:42 -0500

    [+] 02 Variables: Clase Scanner (Entrada datos por terminal)

--- src/Variables.java
+++ src/Variables.java
@@ -1,18 +1,16 @@
-// 02 Variables: Concatenacion de variables
-import java.lang.*;
+// 02 Variables: Clase Scanner (Entrada de datos por terminal)
+import java.util.Scanner;
 
 public class Variables {
     public static void main( String args[] ) throws Exception {
-        var i = 4;      // No permite: var i = 4, j = 5;
-        var j = 5;
-        String name = "Juan";
-        String profession = "Desarrollador";
+        //  Scanner es la clase que nos permitira obtener entradas de datos por la terminal
+        Scanner sc = new Scanner( System .in );         //  System clase de Java que nos permite accede a la terminal a traves de in
 
-        System .out .println( name + profession );      // JuanDesarrollador
-        System .out .println( i + j );                  // 9
-        System .out .println( i + j + name );           // 9Juan    Evalua de Izquierda a Derecha
-        System .out .println( name + i + j );           // Juan45   Contexto cadena: Si de derecha a izquierda primero encuentra una cadena, todo lo demas lo concaterara como cadena
-        System .out .println( name + ( i + j ) );       // Juan9    El contexto cadena se puede romper haciendo uso de los parentesis, aumentando la prioridad en las operaciones dentro de ellos
+        System .out .print( "Ingrese su nombre: " );
+        var nombre = sc .nextLine();                    //  nextLine: Lee una linea completa de la terminal
 
+        System .out .print( nombre + " que profesion tienes: " );
+        var profesion = sc .nextLine();                 //  nextLine: Lee una linea completa de la terminal
+        System .out .println( "Wow! " + nombre + "\nEres " + profesion + ", sensacional." );
     }
 }
