aa5021fdd6f174a91537436bf8ca3517d2ef0202
Juan C Jimenez G <jcjimenez29@misena.edu.co>
2021-07-15 16:55:37 -0500

    [+] Tipos Primitivos: Boolean

---
+++ src/VariableBoolean.java
@@ -0,0 +1,9 @@
+public class VariableBoolean {
+    public static void main( String args[] ) {
+        boolean estaActivo = true;
+        System .out .println( "estaActivo =  " + estaActivo );
+
+        var estaDisponible = false;
+        System .out .println( "estaDisponible = " + estaDisponible );
+    }
+}
