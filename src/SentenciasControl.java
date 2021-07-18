c633631f0682e5564714fd7acab950a37c11f9cd
Juan C Jimenez G <jcjimenez29@misena.edu.co>
2021-07-18 12:52:40 -0500

    [+] Sentencias de control: Switch

--- src/SentenciasControl.java
+++ src/SentenciasControl.java
@@ -2,14 +2,50 @@ import java.util.Scanner;
 
 public class SentenciasControl {
     public static void main( String args[] ) {
-        estacionDelAno();
+        estacionDelAno_ifElseIf();
+        estacionDelAno_Switch();
     }
 
-    public static void estacionDelAno() {
+    public static void estacionDelAno_Switch() {
+        System.out.println( "Usando Switch" );
+        Scanner sc = new Scanner( System .in );
+        var estacion = "";
+
+        System.out.print( "  Digite el numero del mes deseado: " );
+        var mes = sc .nextInt();
+
+        switch( mes ) {
+            case 1:
+            case 2:
+            case 12:
+                estacion = "Invierno";
+                break;
+            case 3:
+            case 4:
+            case 5:
+                estacion = "Primavera";
+                break;
+            case 6: case 7: case 8:
+                estacion = "Verano";
+                break;
+            case 9: case 10: case 11:
+                estacion = "Otonio";
+                break;
+            default:
+                estacion = "Desconocida";
+                break;
+        }
+
+        System .out .println( "  Estacion: " + estacion );
+
+    }
+
+    public static void estacionDelAno_ifElseIf() {
+        System.out.println( "Usando if-else-if" );
         Scanner sc = new Scanner( System .in );
         var estacion = "Desconocida";
 
-        System.out.print( "Digite el numero del mes deseado: " );
+        System.out.print( "  Digite el numero del mes deseado: " );
         var mes = sc .nextInt();
 
         if( mes > 0 && mes <= 12 ) {
@@ -24,11 +60,11 @@ public class SentenciasControl {
                 estacion = "Verano";
             }
             else if ( mes == 9 || mes == 10 || mes == 11 ) {
-                estacion = "Otono";
+                estacion = "Otonio";
             }
         }
 
-        System .out .println( "Estacion: " + estacion );
+        System .out .println( "  Estacion: " + estacion );
 
     }
 }
