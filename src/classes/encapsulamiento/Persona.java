3cb53aff750a1c0d44037819461d207db50fef2a
Juan C Jimenez G <jcjimenez29@misena.edu.co>
2021-07-23 10:59:07 -0500

    [+] Clases: Encapsulando atributos de clase (Getters & Setters)

---
+++ src/classes/encapsulamiento/Persona.java
@@ -0,0 +1,51 @@
+package classes.encapsulamiento;
+
+public class Persona {
+    // Atributos de clase con encapsulados con nivel de acceso de privado
+    private String nombres;
+    private String apellidos;
+    private double sueldo;
+    private boolean eliminado;
+
+    // Constructor
+    public Persona( String nombres, String apellidos, double sueldo, boolean eliminado ) {
+        this .nombres = nombres;
+        this.apellidos = apellidos;
+        this.sueldo = sueldo;
+        this.eliminado = eliminado;
+    }
+
+    // Getters and setters
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres( String nombres ) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos( String apellidos ) {
+        this.apellidos = apellidos;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public boolean isEliminado() {
+        return eliminado;
+    }
+
+    public void setEliminado(boolean eliminado) {
+        this.eliminado = eliminado;
+    }
+
+}
