470ce9d1ee2abc3cd6390c40564113d6847d9b1b
Juan C Jimenez G <jcjimenez29@misena.edu.co>
2021-07-23 11:15:40 -0500

    [+] Clases: Testing usando Getters & Setters

---
+++ src/test/encapsulamiento/TestPersona.java
@@ -0,0 +1,17 @@
+package test.encapsulamiento;
+
+import classes.encapsulamiento.Persona;     // Importa la clase de otro paquete
+
+public class TestPersona {
+    public static void main( String args[] ) throws Exception {
+        Persona p1 = new Persona( "Elisa", "Giraldo", 9000, false );
+        p1 .setNombres( "Elisa Maria" );
+        p1 .setSueldo( 11000 );
+
+        System.out.println( p1. getClass() .getName() );                // Nombre de la clase constructora del objeto
+        System.out.println( " - nombres: " + p1 .getNombres() );
+        System.out.println( " - apellidos: " + p1 .getApellidos() );
+        System.out.println( " - sueldo: " + p1 .getSueldo() );
+        System.out.println( " - eliminado: " + p1 .isEliminado() );
+    }
+}
