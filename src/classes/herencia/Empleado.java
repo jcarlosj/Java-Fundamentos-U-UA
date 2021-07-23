728ba78c04506be7051a1453668c40af21eab597
Juan C Jimenez G <jcjimenez29@misena.edu.co>
2021-07-23 14:50:33 -0500

    [+] Clases: Define cuerpo clase hija 'Empleado' & Testing estado del objeto

--- src/classes/herencia/Empleado.java
+++ src/classes/herencia/Empleado.java
@@ -1,4 +1,50 @@
 package classes.herencia;
 
 // Define clase que hereda de la clase Persona (En Java no existe la herencia multiple)
-public class Empleado extends Persona {}
+public class Empleado extends Persona {
+
+    public static int cantidad;        // 0, cero es el valor por defecto para variables del tipo int
+
+    // Atributos de la clase
+    private int id;
+    private double sueldo;
+
+    // Constructor
+    public Empleado( String nombre, double sueldo ) {
+        super( nombre );
+
+        this.sueldo = sueldo;
+        this .id = ++Empleado .cantidad;
+    }
+
+    // Getters and setters
+    public int getId() {
+        return this .id;
+    }
+
+    public double getSueldo() {
+        return this .sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        // StringBuilder: Almacena cadenas de caracteres.
+        StringBuilder sb = new StringBuilder();
+
+        // StringBuilder: Su tamaño y contenido pueden modificarse. Los objetos de éste tipo son mutables. Esto es una diferencia con los String
+        sb .append( getClass() .getSimpleName() ) .append( "[ " );
+        sb .append( "id = " ) .append( this .id + ", " );
+        sb .append( "sueldo = " ) .append( this .sueldo + ", "  );
+        sb .append( super .toString() );        //  Agregamos los datos de la clase padre
+        sb .append( " ]" );
+
+        return sb .toString();
+    }
+
+    
+
+}
--- src/test/herencia/TestHerencia.java
+++ src/test/herencia/TestHerencia.java
@@ -4,7 +4,10 @@ import classes.herencia.Empleado;
 
 public class TestHerencia {
     public static void main( String[] args ) throws Exception {
-        Empleado e1 = new Empleado();
+        Empleado e1 = new Empleado( "Juan", 9000 );
+        Empleado e2 = new Empleado( "Germán", 12000 );
+
         System.out.println( e1 );           //  Obtenemos detalle de la clase
+        System.out.println( e2 );           //  Obtenemos detalle de la clase
     }
 }
