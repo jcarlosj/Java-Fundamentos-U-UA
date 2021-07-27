a11108c16201707f0a63839512d346dff8051d3e
Juan C Jimenez G <jcjimenez29@misena.edu.co>
2021-07-27 11:16:41 -0500

    [+] Clases: Implementacion del modificador 'final' en variable o atributo (constante)

---
+++ src/classes/modificador_final/Final.java
@@ -0,0 +1,9 @@
+package classes.modificador_final;
+
+public class Final {
+    public static void main(String[] args) throws Exception {
+        // modificador final en variables o atributos
+        final int edad = 29;            // Define una constante
+        System.out.println( "edad: " + edad );
+    }
+}
