10229a585f344e36cec10f31b36e68ef984d575e
Juan C Jimenez G <jcjimenez29@misena.edu.co>
2021-07-28 13:32:42 -0500

    [+] Arreglos: Sobrecarga de metodos estaticos para imprimir matrices

--- src/arrays/Matrices.java
+++ src/arrays/Matrices.java
@@ -2,35 +2,41 @@ package arrays;
 
 public class Matrices {
 
+    public static void imprimir( Object matriz[][] ) {
+        // ? Iteramos los valores del array (foreach)
+        System.out.println( "Elementos: " );
+        for( Object[] filas : matriz ) {
+            for( Object value : filas ) {
+                System.out.print( "   " + value + " " );
+            }
+            System.out.println( "" );
+        }
+    }
+
+    public static void imprimir( int matriz[][] ) {
+        // ? Iteramos los valores del array (foreach)
+        System.out.println( "Elementos: " );
+        for( int[] filas : matriz ) {
+            for( int value : filas ) {
+                System.out.print( value + " " );
+            }
+            System.out.println( "" );
+        }
+    }
+
     public static void tipoObjeto() {
         Persona personas[][] = new Persona[ 3 ][ 2 ];
 
         System.out.println( " ==== Matriz: de tipo referenciado ==== " );
         System.out.println( "personas: " + personas );
 
-        // ? Iteramos los valores del array (foreach)
-        System.out.println( "Elementos: " );
-        for( Persona[] filas : personas ) {
-            //System.out.print( filas + " " );
-            for( Persona value : filas ) {
-                System.out.print( value + " " );
-            }
-            System.out.println( "" );
-        }
+        imprimir( personas );
 
         // ! Modificamos el valor del objeto que por defecto sera null
         personas[ 0 ][ 1 ] = new Persona( "Juan" );
         personas[ 1 ][ 0 ] = new Persona( "Sofia" );
 
-        // ? Iteramos los valores del array (forma tradicional)
-        System.out.println( "Elementos: " );
-        for( int i = 0; i < personas.length; i++ ) {
-            for( int j = 0; j < personas[ i ] .length; j++ ) {
-                System.out.print( personas[ i ][ j ] + " " );
-            }
-            System.out.println( "" );
-        }
-
+        imprimir( personas );
 
     }
 
@@ -45,14 +51,7 @@ public class Matrices {
             { "Naranja", fruta ,"Fresa", "Limon" }
         };         // Sintaxis resumida
 
-        // ? Iteramos los valores del array (forma tradicional)
-        System.out.println( "Elementos: " );
-        for( int i = 0; i < todasLasFrutas.length; i++ ) {
-            for( int j = 0; j < todasLasFrutas[ i ] .length; j++ ) {
-                System.out.print( todasLasFrutas[ i ][ j ] + " " );
-            }
-            System.out.println( "" );
-        }
+        imprimir( todasLasFrutas );
 
     }
 
@@ -62,28 +61,13 @@ public class Matrices {
         System.out.println( " ==== Matriz: de tipo primitivo ==== " );
         System.out.println( "numeros: " + numeros );
 
-        // ? Iteramos los valores del array (foreach)
-        System.out.println( "Elementos: " );
-        for( int[] filas : numeros ) {
-            //System.out.print( filas + " " );
-            for( int value : filas ) {
-                System.out.print( value + " " );
-            }
-            System.out.println( "" );
-        }
+        imprimir( numeros );
 
         // ! Modificamos el valor de uno de los indices del array
         numeros[ 1 ][ 1 ] = 9;
         numeros[ 2 ][ 0 ] = 3;
 
-        // ? Iteramos los valores del array (forma tradicional)
-        System.out.println( "Elementos: " );
-        for( int i = 0; i < numeros.length; i++ ) {
-            for( int j = 0; j < numeros[ i ] .length; j++ ) {
-                System.out.print( numeros[ i ][ j ] + " " );
-            }
-            System.out.println( "" );
-        }
+        imprimir( numeros );
 
         try {
             // ! Modificamos el valor de un indice que no existe en el array
